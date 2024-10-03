c660723402e0c041bf364ef4866ef56e4336976f
Bruno Carvalho <bruno.oak23@gmail.com>
2024-10-02 21:07:06 -0300

    update

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,32 +1,11 @@
-async function listaVídeos() {
-    const conexao = await fetch("http://localhost:3000/videos")
-    const conexaoConvertida = await conexao.json()
+async function listaVideos(){
+    let conexao = await fetch("http://localhost:3000/videos")
+    let conexaoConvertida = await conexao.json();
+
     console.table(conexaoConvertida)
-
     return conexaoConvertida
 }
 
-async function criaVideo(video, imagem, url, descricao ) {
-    const conexao = await fetch("http://localhost:3000/videos", {
-        method:"POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body:JSON.stringify({
-            video: video,
-            imagem: imagem,
-            url: url,
-            descricao: `${descricao} mil visualizações`
-        })
-    })
-
-    const conexaoConvertida = await conexao.json();
-    return conexaoConvertida
+export const conectaApi ={
+    listaVideos
 }
-
-
-export const conectaApi = {
-    listaVídeos,
-    criaVideo
-}
-
